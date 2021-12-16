2836ef53d43eae38e47595110f5cdae8fbc4fefc
AZHAR ANSARI <azhar@AZHARs-MacBook-Air.local>
2021-12-16 13:42:57 +0530

    create header for shop and contat navigation bar..next gonna create sign in in navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import HomePage from "./components/pages/homepage.component";
 import ShopPage from "./components/pages/shop/shop.component";
 import { Route, Switch } from "react-router-dom";
-
+import Header from "./components/header/header.component";
 // const HatsPage = () => (
 //   <div>
 //     <h1>Hats Page</h1>
@@ -12,6 +12,8 @@ import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <div>
+      <Header />
+
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
